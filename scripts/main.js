24b5be1a8e3f0b9cbaf25b3877b9032dac4907fc
Alfina Balqis Nurzaharani <alfinabalqis28@gmail.com>
2023-12-28 15:34:53 +0700

    feat: add content

--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,83 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
-$(document).ready(function() {
-  AOS.init( {
+$(document).ready(function () {
+  AOS.init({
     // uncomment below for on-scroll animations to played only once
-    // once: true  
+    // once: true
   }); // initialize animate on scroll library
 });
 
 // Smooth scroll for links with hashes
-$('a.smooth-scroll')
-.click(function(event) {
+$("a.smooth-scroll").click(function (event) {
   // On-page links
   if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
     location.hostname == this.hostname
   ) {
     // Figure out element to scroll to
     var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
     // Does a scroll target exist?
     if (target.length) {
       // Only prevent default if animation is actually gonna happen
       event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000,
+        function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
     }
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+/* Countdown */
+document.addEventListener("DOMContentLoaded", () => {
+  // Tanggal dan waktu target: 14 Februari 2024 pukul 08.00 WIB
+  var targetDate = new Date("2024-02-14T08:00:00+07:00");
 
-  // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+  // Unix timestamp (in seconds) untuk tanggal dan waktu target
+  var timestampTargetDate = Math.floor(targetDate.getTime() / 1000);
 
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
+  var flipdown = new FlipDown(timestampTargetDate)
 
     // Start the countdown
     .start()
 
     // Do something when the countdown ends
     .ifEnded(() => {
-      console.log('The countdown has ended!');
+      console.log("The countdown has ended!");
     });
 
-  // Toggle theme
-  var interval = setInterval(() => {
-    let body = document.body;
-    body.classList.toggle('light-theme');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
-  }, 5000);
-
   // Show version number
-  var ver = document.getElementById('ver');
+  var ver = document.getElementById("ver");
   ver.innerHTML = flipdown.version;
 });
+
+// Audio
+function toggleAudio() {
+  var audio = document.getElementById('myAudio');
+  var button = document.querySelector('.play-pause-button');
+  if (audio.paused) {
+      audio.play();
+      button.classList.add('playing');
+  } else {
+      audio.pause();
+      button.classList.remove('playing');
+  }
+}
